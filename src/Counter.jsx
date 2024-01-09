ea225b3a1c5219ff3d16f542a08b8344188f7ad0
urumadlavasu <urumadlavasu04@gmail.com>
2024-01-09 15:14:39 +0530

    sfd

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,8 +1,17 @@
 import React from "react";
 function Counter(){
+    let [c,setC] = React.useState(0)
+    function inc(){
+        setC(c+1)
+    }
+    function dec(){
+        setC(c-1)
+    }
     return(
         <div className="mybox" >
-            <h1>Counter:</h1>
+            <h1>Counter:{c}</h1>
+            <button onClick={inc} >Inc</button>
+            <button onClick={dec}>Dec</button>
         </div>
     )
 }
